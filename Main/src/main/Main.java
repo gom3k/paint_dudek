23d5c77df13086dd320f5a943d80107cb0b72685
student <student@240-09.INFORMATYKA.local>
2016-12-01 10:56:02 +0100

    Lab8(lab nr.3), zad 2

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -2,15 +2,14 @@ package main;
 
 import javax.swing.JFrame;
 
-
 public class Main{
     public static void main(String[] args) {
-    JFrame frame = new JFrame();
-    Panel panel = new Panel();
-    frame.setSize(500, 300);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(panel);
-    frame.setVisible(true);
+        JFrame frame = new JFrame();
+        Panel panel = new Panel();
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
     }
 }
 
