1a5f2108365b1e8a21dd4861eb11d5661799f158
student <student@240-09.INFORMATYKA.local>
2016-12-01 10:55:27 +0100

    Lab8(lab nr.3), zad 2

--- Main/src/main/Panel.java
+++ Main/src/main/Panel.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
 public class Panel extends JPanel implements MouseListener{
+    private int x = -1;
+    private int y = -1;
+    
     public Panel(){
         addMouseListener(this);
     }
@@ -16,6 +20,17 @@ public class Panel extends JPanel implements MouseListener{
     public void mouseReleased(MouseEvent e){}
     
     public void mouseClicked(MouseEvent e){
-        JOptionPane.showMessageDialog(null,e.getPoint().toString());
+        x = e.getX();
+        y = e.getY();
+        repaint();
+//        JOptionPane.showMessageDialog(null,e.getPoint().toString());
+    }
+    
+    public void paint(Graphics g){
+        g.clearRect(0, 0, getSize().width, getSize().height);
+        g.setColor(Color.RED);
+        if(x!=-1 && y!=-1){
+            g.drawOval(x-25, y-25, 50, 50);
+        }
     }
 }
