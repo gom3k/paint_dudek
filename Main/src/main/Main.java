bcc673d4d04db3861a491fb9c3b1b9feaea9a42e
student <student@240-09.INFORMATYKA.local>
2016-12-01 11:06:54 +0100

    Lab8(lab nr.3), zad 3

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -5,11 +5,10 @@ import javax.swing.JFrame;
 public class Main{
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        Panel panel = new Panel();
+        Panel panel = new Panel() {};
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(panel);
         frame.setVisible(true);
     }
 }
-
--- Main/src/main/Panel.java
+++ Main/src/main/Panel.java
@@ -4,26 +4,39 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel implements MouseListener{
+public class Panel extends JPanel implements MouseListener, MouseMotionListener{
     private int x = -1;
     private int y = -1;
     
     public Panel(){
         addMouseListener(this);
+        addMouseMotionListener(this);
     }
+    // MouseListener
     public void mouseExited(MouseEvent e){}
     public void mouseEntered(MouseEvent e){}
     public void mousePressed(MouseEvent e){}
     public void mouseReleased(MouseEvent e){}
-    
     public void mouseClicked(MouseEvent e){
         x = e.getX();
         y = e.getY();
         repaint();
 //        JOptionPane.showMessageDialog(null,e.getPoint().toString());
+    }
+    // MouseMotionListener
+    public void mouseMoved(MouseEvent e){
+        x = e.getX();
+        y = e.getY();
+        repaint();
+    }
+    public void mouseDragged(MouseEvent e) {
+//        x = e.getX();
+//        y = e.getY();
+//        repaint();
     }
     
     public void paint(Graphics g){
@@ -33,4 +46,6 @@ public class Panel extends JPanel implements MouseListener{
             g.drawOval(x-25, y-25, 50, 50);
         }
     }
+
+
 }
