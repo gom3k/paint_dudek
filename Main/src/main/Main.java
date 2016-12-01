d210856555595451a323dd7b452303a1e0927fd9
student <student@240-09.INFORMATYKA.local>
2016-12-01 11:43:38 +0100

    Lab8(lab nr.3), zad 4, linia

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Main{
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        Panel panel = new Panel() {};
+        Panel panel = new Panel(){};
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(panel);
--- Main/src/main/Panel.java
+++ Main/src/main/Panel.java
@@ -2,15 +2,14 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Panel extends JPanel implements MouseListener, MouseMotionListener{
     private int x = -1;
     private int y = -1;
+    private int a;
+    private int b;
     
     public Panel(){
         addMouseListener(this);
@@ -19,31 +18,35 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener{
     // MouseListener
     public void mouseExited(MouseEvent e){}
     public void mouseEntered(MouseEvent e){}
-    public void mousePressed(MouseEvent e){}
-    public void mouseReleased(MouseEvent e){}
-    public void mouseClicked(MouseEvent e){
+    public void mousePressed(MouseEvent e){
         x = e.getX();
         y = e.getY();
-        repaint();
+//        repaint();
+    }
+    public void mouseReleased(MouseEvent e){}
+    public void mouseClicked(MouseEvent e){
+//        x = e.getX();
+//        y = e.getY();
+//        repaint();
 //        JOptionPane.showMessageDialog(null,e.getPoint().toString());
     }
     // MouseMotionListener
     public void mouseMoved(MouseEvent e){
-        x = e.getX();
-        y = e.getY();
-        repaint();
-    }
-    public void mouseDragged(MouseEvent e) {
 //        x = e.getX();
 //        y = e.getY();
 //        repaint();
+    }
+    public void mouseDragged(MouseEvent e) {
+        a = e.getX();
+        b = e.getY();
+        repaint();
     }
     
     public void paint(Graphics g){
         g.clearRect(0, 0, getSize().width, getSize().height);
         g.setColor(Color.RED);
         if(x!=-1 && y!=-1){
-            g.drawOval(x-25, y-25, 50, 50);
+            g.drawLine(x, y, a, b);
         }
     }
 
